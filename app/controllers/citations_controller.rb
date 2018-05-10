be71cffa576f23834b97572b9c3317583368641b
bochen421 <arkadiusz.bochenek@gmail.com>
2018-05-11 01:00:22 +0200

    Add controller, routes, migration, seed and view

---
+++ app/controllers/citations_controller.rb
@@ -0,0 +1,25 @@
+class CitationsController < ApplicationController
+  def index
+    @citations = Citation.all.order(id: :desc)
+  end
+
+  def new
+    @citation = Citation.new
+  end
+
+  def create
+    @citation = Citation.new(citation_params)
+    if @citation.save
+      redirect_to citations_path, success: t('.citation_created')
+    else
+      flash.now[:danger] = t('.failed_to_create_citation')
+      render :new
+    end
+  end
+
+  private
+
+  def citation_params
+    params.require(:citation).permit(:body)
+  end
+end
---
+++ app/models/citation.rb
@@ -0,0 +1,3 @@
+class Citation < ApplicationRecord
+  validates :body, presence: true
+end
